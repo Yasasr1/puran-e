9a8bca838997871387700622baae9c981195d5ff
emsuds <2017is019@stu.ucsc.cmb.ac.lk>
2019-11-30 15:30:30 +0530

    add routings

--- puran-e/src/App.js
+++ puran-e/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Home from './Components/Home/Home';
 import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Payment from './Components/Payment/Payment'
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/payment" exact component={Payment} />
+
+    </Router>
+   
   );
 }
 
---
+++ puran-e/src/Components/Payment/Payment.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+export default class Payment extends Component {
+    render() {
+        return (
+       <div>
+           <p>hi</p>
+        </div>
+
+        )
+    }
+}
