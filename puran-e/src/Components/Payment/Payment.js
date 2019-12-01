9a287dbf391e5bfdf2a88a22850655c4ba74f752
emsuds <2017is019@stu.ucsc.cmb.ac.lk>
2019-12-01 11:14:41 +0530

    payment

--- puran-e/src/Components/Payment/Payment.js
+++ puran-e/src/Components/Payment/Payment.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
+import Header from '../Header/Header'
+import { FormGroup } from 'react-bootstrap'
 
 
 export default class Payment extends Component {
     render() {
         return (
        <div>
-           <p>hi</p>
+           <Header/>
+           <div class="container mt-8"></div>
+           <div class="row">
+           <div class="col-md-12">
+               <center>
+               <div class="card text-center">
+  <div class="card-header">
+    Donations
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Please Select Your Payment Type</h5>
+    <p class="card-text">Approved by the Central Bank of Sri Lanka</p>
+    <FormGroup>
+        <label>Name On Card </label>
+        <input type="text"></input>
+    </FormGroup>
+ 
+    <a href="#" class="btn btn-primary">Pay</a>
+  </div>
+  <div class="card-footer text-muted">
+    Any Assistance Please call our hotline
+  </div>
+</div></center></div></div>
+           
         </div>
 
         )
